125ad1421975f0954c13cc6940db2f40f41c021e
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:36 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/badges/AoBadgesStyle.java
+++ src/main/java/com/aoapps/badges/AoBadgesStyle.java
@@ -1,6 +1,6 @@
 /*
  * ao-badges - Badges from shields.io bundled into a reusable JSP-based web component.
- * Copyright (C) 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  * 	   Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-badges.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.badges;
 
 import com.aoapps.web.resources.registry.Group;
